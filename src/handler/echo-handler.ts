a1f62c3f1f73cbcdd9a9e987bbe6916086046c96
Ying Qiao <qiaoying@hotmail.com>
2020-07-16 14:31:42 -0400

    rm index for proto

--- src/handler/echo-handler.ts
+++ src/handler/echo-handler.ts
@@ -6,7 +6,6 @@ import { EchoService, IEchoServer } from "../proto/echo/echo_grpc_pb";
 
 class EchoHandler implements IEchoServer {
 
-
     getEcho = (call: grpc.ServerUnaryCall<Said>, 
             callback: grpc.sendUnaryData<Repeated>) : void => {
 
--- src/proto/index.ts
+++
@@ -1,6 +0,0 @@
-import "../proto/echo/echo_pb";
-import "../proto/echo/echo_grpc_pb";
-
-export const protoIndex: any =  (): void => {
-    
-}
